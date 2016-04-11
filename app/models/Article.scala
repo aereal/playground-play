9121016a5a111eaec4cf6b9833e6dce0cd97db75
aereal <aereal@aereal.org>
2016-04-11 17:49:39 +0900

    resolveById

--- app/models/Article.scala
+++ app/models/Article.scala
@@ -6,9 +6,17 @@ case class Article(
 )
 
 object ArticleRepository {
+  var lastId: Int = 1
   var repo: List[Article] = List()
 
-  def save(article: Article) = {
+  def save(article: Article): Int = {
+    val insertedId = lastId
+    lastId = lastId + 1
     repo = repo ::: List(article)
+    insertedId
+  }
+
+  def resolveById(id: Int): Article = {
+    repo(id - 1)
   }
 }
--- test/ArticleSpec.scala
+++ test/ArticleSpec.scala
@@ -1,10 +1,17 @@
 import org.scalatest.FunSuite
 
-import models.Article
+import models.{Article, ArticleRepository}
 
 class ArticleSpec extends FunSuite {
   test("Article is a ...") {
     val article = Article(title = "title", body = "body")
     assert(article.title == "title")
+  }
+
+  test("ArticleRepository") {
+    val article = Article(title = "title", body = "body")
+    val articleId = ArticleRepository.save(article)
+    val resolved: Article = ArticleRepository.resolveById(articleId)
+    assert(article == resolved)
   }
 }
