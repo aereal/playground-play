e4137184f323d8cfaa66e8a503678f0d43e7a9e0
aereal <aereal@aereal.org>
2016-04-11 18:35:57 +0900

    ArticleRepository is a Seq

--- app/models/Article.scala
+++ app/models/Article.scala
@@ -1,14 +1,22 @@
 package models
 
+import scala.collection.immutable.Seq
+
 case class Article(
   id: Int,
   title: String,
   body: String
 )
 
-object ArticleRepository {
+object ArticleRepository extends Seq[Article] {
   var lastId: Int = 1
   private var repo: List[Article] = List()
+
+  def apply(idx: Int) = repo(idx)
+
+  def length = repo.length
+
+  def iterator: Iterator[Article] = repo.toIterator
 
   def all: List[Article] = repo
 
