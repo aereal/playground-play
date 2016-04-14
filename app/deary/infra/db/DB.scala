fecfe18d40d1c76d4dc0d4dc1290ac1bcb589020
aereal <aereal@aereal.org>
2016-04-14 22:18:09 +0900

    Bye DBComponent

--- app/deary/infra/db/DB.scala
+++ app/deary/infra/db/DB.scala
@@ -4,12 +4,6 @@ import slick.backend.DatabaseConfig
 import slick.driver.JdbcProfile
 import slick.profile.BasicProfile
 
-trait DBComponent[P <: BasicProfile] {
-  val dbConfig: DatabaseConfig[P]
-
-  def db = dbConfig.db
-}
-
 trait UsesDB {
   def dbConfig: DatabaseConfig[JdbcProfile]
   def db: JdbcProfile#Backend#Database
